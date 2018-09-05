3334f617eed72c28d897babf7425875fda2ec767
Michael Burke <michaelburke@michaels-macbook-pro-2.local>
2018-09-04 21:13:03 -0400

    Old files were corrupted had to make a new 2.10 version

--- youAreAwesomeReal/ViewController.swift
+++ youAreAwesomeReal/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    var index = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("The View Loaded!")
     }
-
-
+    
+    @IBAction func showMessagePress(_ sender: UIButton) {
+        let messages = ["You are fantastic!", "You are great!", "You are amazing!"]
+        
+        if index == messages.count-1 { index = 0 } else { index = index + 1}
+        
+        /*index = index + 1
+         if index == 3 { index = index - 3} */
+        
+        messageLabel.text = messages[index]
+        
+        /* let message1 = "You are Awesome!"
+         let message2 = "You Are Great!"
+         let message3 = "You Are Amazing!"
+         
+         
+         if messageLabel.text == message1
+         { messageLabel.text = message2}
+         else if messageLabel.text == message2 {
+         messageLabel.text = message3}
+         else { messageLabel.text = message1} */
+        
+    }
+    
 }
-
